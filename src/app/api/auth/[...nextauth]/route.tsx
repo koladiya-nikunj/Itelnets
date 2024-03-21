add3e4cac87895d4246e435ad8288e598784da8b
koladiya-nikunj <koladiyanikunj124@gmail.com>
2024-03-21 13:46:57 +0530

    Data send to backend complete

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,15 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { randomBytes } from "crypto";
+import axios from 'axios'
+
+
+
+interface BackendData {
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+}
 
 const generateSecret = () => {
   return randomBytes(32).toString("hex");
@@ -18,6 +27,32 @@ const handler = NextAuth({
     async signIn({ user }) {
       const { name, email, image } = user;
       console.log("Successful login:", { name, email, image });
+
+      // Function to send data to the backend
+      // Function to send data to the backend
+      async function sendDataToBackend(data: BackendData) {
+        try {
+          const response = await axios.post('http://localhost:5000/api/auth/callback/google/', data);
+          console.log('Response status:', response.status); // Log the response status
+          if (response.status === 200 || response.status === 201) { // Adjust the condition to include 201 Created status
+            console.log('Data sent successfully to backend');
+          } else {
+            throw new Error('Failed to send data to backend');
+          }
+        } catch (error) {
+          console.error('Error sending data to backend:', error);
+          throw error;
+        }
+      }
+
+      // Call the function to send data to backend
+      try {
+        await sendDataToBackend({ name, email, image });
+      } catch (error) {
+        console.error('Error sending data to backend:', error);
+        // Handle error, show error message to the user, etc.
+      }
+
 
       return Promise.resolve('/profile');
     }
