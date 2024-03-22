a486fbf78155e569745e74b7e3ca82dee8f3151d
koladiya-nikunj <koladiyanikunj124@gmail.com>
2024-03-22 12:56:58 +0530

    Reset Password page added

--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Form, Input, Button, notification } from 'antd';
 import { MailOutlined, LockOutlined, CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import axios from 'axios'
-import { signIn } from 'next-auth/react';
-import Image from 'next/image';
+
 
 const Register = () => {
   const [form] = Form.useForm();
@@ -69,7 +67,8 @@ const Register = () => {
         onFinish={onFinish}
         className='bg-white shadow-md rounded-lg px-10 pt-8 pb-8 mb-4 w-full max-w-md'
       >
-        <h1 className='font-bold text-3xl p-2 mb-4 text-center'>Login</h1>
+        <h1 className='font-bold text-3xl p-2 text-center'>Enter your email to</h1>
+        <h1 className='font-bold text-3xl p-2 mb-4 text-center'>Reset Password</h1>
         <Form.Item
           name='email'
           rules={[{ required: true, type: 'email', message: 'Please enter your email!' }]}
@@ -79,22 +78,6 @@ const Register = () => {
             placeholder='Email'
           />
         </Form.Item>
-        <Form.Item
-          name='password'
-          rules={[
-            { required: true, message: 'Please enter your password!' },
-            {
-              pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-              message: 'Password must be at least 8 characters with A-Z, a-z, 0-9, and special characters @$!%*#?&',
-            },
-          ]}
-        >
-          <Input.Password
-            prefix={<LockOutlined className='site-form-item-icon' />}
-            placeholder='Password'
-            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-          />
-        </Form.Item>
         <Form.Item>
           <Button
             type='primary'
@@ -102,27 +85,13 @@ const Register = () => {
             loading={isSubmitting}
             className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded focus:outline-none focus:shadow-outline w-full'
           >
-            Login
+            Reset
           </Button>
           <div className='text-center mt-4'>
-            Don't have an account ?{' '}
-            <Link className=' text-blue-600 font-bold pl-1' href='/register'>
-              Register
-            </Link>
-          </div>
-          <div className='text-center mt-2'>
-           Reset password ?{' '}
-            <Link className=' text-blue-600 font-bold pl-1' href='/resetpassword'>
+           Back to login ?{' '}
+            <Link className=' text-blue-600 font-bold pl-1' href='/login'>
               Click
             </Link>
-          </div>
-          <div className='text-center mt-4'>
-            <button className="" type="button">
-              <span onClick={() => signIn("google")} className="flex bg-gray-100 hover:bg-gray-200 p-2 px-8 rounded-lg">
-                <Image className='mx-2 mr-8' src="/google.png" alt="Google Logo" height={24} width={24} />
-                <span className="mr-5 font-semibold"><a className='hover:text-black'>Continue with Google</a></span>
-              </span>
-            </button>
           </div>
         </Form.Item>
       </Form>
